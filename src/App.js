b99b37847e13d07b74443ca07a80146ea80ea6b4
Vitor Ferreira Iassia <iassiavitor@gmail.com>
2021-10-04 14:55:55 -0300

    Ciclo de Methods do React

--- src/App.js
+++ src/App.js
@@ -5,29 +5,59 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   state = {
-    posts: [
-      {
-        id: "1",
-        title: "Titulo 1",
-        body: "Corpo do post"
-      },
-      {
-        id: "2",
-        title: "Titulo 2",
-        body: "Corpo do post"
-      },
-      {
-        id: "3",
-        title: "Titulo 3",
-        body: "Corpo do post"
-      },
-    ]
+    counter: 0,
+    posts: [{
+      id: "1",
+      title: "Titulo 1",
+      body: "Corpo do post"
+    },
+    {
+      id: "2",
+      title: "Titulo 2",
+      body: "Corpo do post"
+    },
+    {
+      id: "3",
+      title: "Titulo 3",
+      body: "Corpo do post"
+    },]
   };
 
+  timeout = null;
+
+  handleTimeOut = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "Titulo mudou";
+    this.timeout = setTimeout(() => {
+      this.setState({
+        posts, counter: counter + 1
+      });
+    }, 1000)
+  }
+
+  componentDidUpdate() {
+    this.handleTimeOut()
+    console.log("Componente teve estados modificados")
+  }
+
+  componentDidMount() {
+    this.handleTimeOut()
+    console.log("Tela Carregada e Valores atribuidos")
+  }
+  componentWillUnmount() {
+    clearTimeout(this.timeout)
+    console.log("Componente desmontado")
+  }
+
   render() {
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
+
+
+
+
     return (
       <div className="App">
+        <h1> {counter} </h1>
         {posts.map(post => (
           <div key={post.id}>
             <h1 >{post.title}</h1>
